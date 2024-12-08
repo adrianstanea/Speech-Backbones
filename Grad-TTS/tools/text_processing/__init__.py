1bca04ba10aa032883f73a992fdffafb7ac417d1
Bogdan Stanea <adrianstanea1@gmail.com>
2024-12-08 21:21:17 +0200

    tools/text_processing: add text processing tools
    - Replace CMUDict text processing which only works with English text
    with a more general text processing module that can be used with
    a wider range of languages (including Romanian).
    
    Signed-off-by: Bogdan Stanea <adrianstanea1@gmail.com>

---
+++ Grad-TTS/tools/text_processing/__init__.py
@@ -0,0 +1,33 @@
+from phonemizer.backend import EspeakBackend
+
+from symbols import symbols, _symbol_to_id
+import cleaners as cleaners
+
+global_backend = EspeakBackend(
+    language='ro',
+    preserve_punctuation=True,
+    with_stress=True, # TBD - might want to use False
+    words_mismatch='ignore',
+)
+
+# text = "Aceasta este o propoziție în limba română."
+# phonemes = backend.phonemize([text], strip=False)[0]
+# print(phonemes)
+
+def text_to_phoneme(text : str, phoemizer : EspeakBackend):
+    text = cleaners.to_ascii(text)
+    text = cleaners.to_lowercase(text)
+    phonemes = phoemizer.phonemize([text], strip=False)[0]
+    phonemes = cleaners.collapse_whitespace(phonemes)
+    return phonemes
+
+
+def cleaned_text_to_sequence(cleaned_text):
+    '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+    Args:
+        text: string to convert to a sequence
+    Returns:
+        List of integers corresponding to the symbols in the text
+    '''
+    sequence = [_symbol_to_id[symbol] for symbol in cleaned_text]
+    return sequence
---
+++ Grad-TTS/tools/text_processing/symbols.py
@@ -0,0 +1,21 @@
+"""
+From: https://github.com/keithito/tacotron
+
+Defines the set of symbols used in text input to the model.
+"""
+
+
+_pad        = '_'
+_punctuation = ';:,.!?¡¿—…"«»“” '
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ̃"
+
+# Export all symbols:
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+
+# Special symbol ids
+SPACE_ID = symbols.index(" ")
+
+# Mappings from symbol to numeric ID and vice versa:
+_symbol_to_id = {s: i for i, s in enumerate(symbols)}
+_id_to_symbol = {i: s for i, s in enumerate(symbols)}
